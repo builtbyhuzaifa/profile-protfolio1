b3d7e9bb66917b9e39c8049a43cab2ce7c8ab172
Mohd Huzaifa <mohdhuzaifasaifi95@gmail.com>
2026-09-18 20:26:49 +0530

    Myprotfolio

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+let btn = document.getElementById("hirebtn");
+
+btn.addEventListener("click", function(){
+    alert("Hello Sir!")
+})
+
+let themebutton = document.getElementById("theme");
+
+themebutton.addEventListener("click", function(){
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")){
+        themebutton.innerHTML = "Dark Mode";
+    }
+    else {
+        themebutton.innerHTML = "Light Mode"
+    }
+})
+
+let texttotype = "Web Developer. ";
+
+let typewriterelemet = document.getElementById("typing");
+
+let charIndex = 0;
+function typeText() {
+    if (charIndex < texttotype.length){
+        typewriterelemet.innerText += texttotype.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeText, 150);
+    }
+}
+typeText();
